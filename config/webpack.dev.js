241f4c7f8eab76e9c6556d8644758ee18d7703c2
monkick <saruyama.monki@gmail.com>
2016-09-24 18:45:53 +0900

    support sass

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -20,5 +20,15 @@ module.exports = webpackMerge (commonConfig, {
     devServer: {
         historyApiFallback: true,
         stats             : 'minimal'
+    },
+
+    module: {
+        loaders: [
+            {
+                test   : /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+            }
+        ]
     }
 });
